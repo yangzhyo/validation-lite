da5098629a62992d3a6705795eed754f4fc6ee3c
John.Z.Yang <john.z.yang@newegg.com>
2017-03-22 17:17:33 +0800

    override ToString method of ValidationResult

--- Validation.Lite/ValidationResult.cs
+++ Validation.Lite/ValidationResult.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Validation.Lite
 {
@@ -13,12 +14,32 @@ namespace Validation.Lite
             ErrorMessages = new List<string>();
         }
 
+        public ValidationResult(bool isValid, string message)
+        {
+            IsValid = isValid;
+            ErrorMessages = new List<string>() { message };
+        }
+
         public void MergeValidationResult(ValidationResult result)
         {
             if (!result.IsValid)
             {
                 IsValid = false;
                 ErrorMessages.AddRange(result.ErrorMessages);
+            }
+        }
+
+        public override string ToString()
+        {
+            if (IsValid)
+            {
+                return "Validate successfully.";
+            }
+            else
+            {
+                string errorMessage = "Validate failed:" + Environment.NewLine;
+                ErrorMessages.ForEach(e => { errorMessage += e + Environment.NewLine; });
+                return errorMessage;
             }
         }
     }
