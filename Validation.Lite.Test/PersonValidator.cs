4f0ec01542d4200b8a728887cef11b7add2f38c8
John.Z.Yang <john.z.yang@newegg.com>
2017-04-07 16:56:23 +0800

    v1.2

---
+++ Validation.Lite.Test/PersonValidator.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Validation.Lite.Test
+{
+    public class PersonValidator : ValidateFor<Person>
+    {
+        public PersonValidator()
+        {
+            this.Field(p => p.Age).ShouldGreaterThan(0)
+                .Field(p => p.Company).ShouldNotNull().ValidateWith(new CompanyValidator());
+        }
+    }
+
+    public class CompanyValidator : ValidateFor<Company>
+    {
+        public CompanyValidator()
+        {
+            this.Field(c => c.Address).ShouldNotEmpty().ShouldPassCustomCheck(CheckAddress);
+        }
+
+        public ValidationResult CheckAddress(string address)
+        {
+            return ValidationResult.Valid;
+        }
+    }
+
+    [TestClass]
+    public class ValidatorTest
+    {
+        [TestMethod]
+        public void Test_Instant_Validator()
+        {
+            Person john = new Person();
+            PersonValidator validator = new PersonValidator();
+            ValidationResult result = validator.Validate(john);
+            Assert.IsFalse(result.IsValid);
+        }
+    }
+}
